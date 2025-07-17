9c9d0f643b3b23f999116135e56ffee4d886ea31
Ana <89231022@student.upr.si>
2025-07-17 19:57:41 +0200

    First commit

---
+++ frontend/src/components/home/Homepage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Homepage(){
+    return(
+        <div className="content-container">
+            <h1>CUBIX</h1>
+            <div className="options">
+                <select>
+                    {/*connect first the backend with the db and the use api and useState for the dropdown*/}
+                </select>
+            </div>
+        </div>
+    )
+}
+export default Homepage;
