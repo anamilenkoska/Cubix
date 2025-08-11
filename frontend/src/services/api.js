5019b3447dd03594f5aa97b23cd980ac0d4085b4
Ana Milenkoska <89231022@student.upr.si>
2025-08-11 22:03:22 +0200

    Final commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios"
 
 const api=axios.create({
-    //baseURL:'http://88.200.63.148:8886',
-    baseURL:process.env.REACT_APP_API_URL || '/api',
+    baseURL:'http://88.200.63.148:8886',
     timeout:2000,
     withCredentials:true
 })
